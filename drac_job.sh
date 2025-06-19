52c561d88594301288ae7c6ab2a33bc1bd9640e7
Nicolas Ong <Nicolas.J.Ong@gmail.com>
2025-06-19 15:43:13 -0600

    added requirements file and slurm job submission file for compute canada/drac

---
+++ drac_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --account=def-nathanst-ab
+#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=1G
+
+module load python/3.10
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+pip install --no-index -r requirements_drac.txt
+
+time python -m a0.parallel.jax_async_play
