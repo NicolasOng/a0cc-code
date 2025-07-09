f78e697cdbdc5c98a99608a8aa4233c6483dad7b
Nicolas Ong <Nicolas.J.Ong@gmail.com>
2025-07-09 12:56:08 -0600

    fixed importing statements

--- drac_job.sh
+++ drac_job.sh
@@ -10,6 +10,6 @@ source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index --upgrade pip
 pip install --no-index -r requirements_drac.txt
 
-time python -m a0.parallel.jax_async_play
-time python3 -m a0.evaluations.state_evaluation.data.make_dataset
-time python3 -m a0.evaluations.state_evaluation.static_states.evaluate_outcome
+time python -m python2.parallel.jax_async_play
+time python3 -m python2.evaluations.state_evaluation.data.make_dataset
+time python3 -m python2.evaluations.state_evaluation.static_states.evaluate_outcome
