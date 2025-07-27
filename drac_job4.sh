adbec7e5749acbd74e56d89230cfbd7bfc3f6cec
Nicolas Ong <Nicolas.J.Ong@gmail.com>
2025-07-26 19:44:19 -0600

    added job for mcts eval on cc

---
+++ drac_job4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account=def-nathanst-ab
+#SBATCH --time=30:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=2G
+
+module load python/3.10
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+pip install --no-index -r requirements_drac.txt
+
+# Set the configuration file path, defaulting to config/config.json if not provided
+CONFIG_FILE="${1:-config/config.json}" 
+
+time python -m scripts.evaluate_mcts "$CONFIG_FILE"
