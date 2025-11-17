16bafec45209525b36d1e11d35e406e1b730d4fb
Nicolas Ong <Nicolas.J.Ong@gmail.com>
2025-11-17 15:54:41 -0700

    changed the sbatch file for CC to include trial number functionality

--- drac_job2.sh
+++ drac_job2.sh
@@ -11,12 +11,16 @@ pip install --no-index --upgrade pip
 pip install --no-index -r requirements_drac.txt
 
 # Set the configuration file path, defaulting to config/config.json if not provided
-CONFIG_FILE="${1:-config/config.json}" 
+CONFIG_FILE="${1:-config/config.json}"
+TRIAL_NO="${2}"
 
-time python -m a0.train.alphazero "$CONFIG_FILE"
-time python -m a0.eval.generate_datasets "$CONFIG_FILE"
-time python -m a0.eval.dataset_evaluation "$CONFIG_FILE"
-time python -m a0.eval.mcts_evaluation "$CONFIG_FILE"
-time python -m a0.eval.training_data "$CONFIG_FILE"
-time python -m a0.eval.plotting "$CONFIG_FILE"
-#time python -m a0.eval.player "$CONFIG_FILE"
+echo "Using configuration file: $CONFIG_FILE"
+echo "Using trial number: $TRIAL_NO"
+
+time python -m a0.train.alphazero "$CONFIG_FILE" "$TRIAL_NO"
+time python -m a0.eval.generate_datasets "$CONFIG_FILE" "$TRIAL_NO"
+time python -m a0.eval.dataset_evaluation "$CONFIG_FILE" "$TRIAL_NO"
+time python -m a0.eval.mcts_evaluation "$CONFIG_FILE" "$TRIAL_NO"
+time python -m a0.eval.training_data "$CONFIG_FILE" "$TRIAL_NO"
+time python -m a0.eval.plotting "$CONFIG_FILE" "$TRIAL_NO"
+#time python -m a0.eval.player "$CONFIG_FILE" "$TRIAL_NO"
