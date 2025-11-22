5a0450ab617d616731d112d8cd5b8327aeb21141
Nicolas Ong <Nicolas.J.Ong@gmail.com>
2025-11-21 18:10:50 -0700

    fixing circular import

--- a0/mcts/nn.py
+++ a0/mcts/nn.py
@@ -4,10 +4,47 @@ from cc.core import Game, Board, Player, Move
 from a0.model import AlphaZeroModel
 from a0.model_utils import board_to_input, Policy
 #from cc.ground_truth import GroundTruth
-from scripts.new_policy_functions import get_value_head_policy, get_policy_head_policy
 
 import jax.numpy as jnp
 import numpy as np
+from numpy.typing import NDArray
+
+def get_value_head_policy(model: AlphaZeroModel, state: Board, moves: list[Move], for_model: bool = False) -> tuple[NDArray[np.float32], Policy]:
+    rotate = state.current_player == Player.PLAYER_O if for_model else False
+
+    values: list[float] = []
+    for move in moves:
+        state.apply_move(move)
+        value, _ = model(jnp.array(board_to_input(state)))
+        values.append(float(value[0][0]))
+        state.undo_move(move)
+
+    p = Policy(len(state.board))
+    p.set_logits_from_moves(moves, values, rotate_180=False)
+    p.set_legal_moves(moves)
+    p.apply_softmax(temperature=1.0, mask=True)
+    if rotate:
+        p.rotate_policy()
+    
+    return p.policy, p
+
+def get_policy_head_policy(model: AlphaZeroModel, state: Board, moves: list[Move], for_model: bool = False) -> tuple[NDArray[np.float32], Policy]:
+    '''
+    Returns the policy distribution over the given moves for the given state using the model.
+    If for_model is True, the policy is rotated according to the model's perspective.
+    '''
+    rotate = state.current_player == Player.PLAYER_O if for_model else False
+
+    _, policy = model(jnp.array(board_to_input(state)))
+
+    p = Policy(len(state.board))
+    p.set_logits(np.array(policy[0]), rotate_180=False)
+    p.set_legal_moves(moves)
+    p.apply_softmax(temperature=1.0, mask=True)
+    if rotate:
+        p.rotate_policy()
+    
+    return p.policy, p
 
 def random_rollout(state: Board, game: Game, max_depth: int) -> tuple[bool, Player | None, Board]:
     current_board = state
