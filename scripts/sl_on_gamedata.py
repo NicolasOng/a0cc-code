a333a16aa2e3c743b71d0ed61d9b6171fa5767e1
Nicolas Ong <Nicolas.J.Ong@gmail.com>
2026-01-12 13:00:14 -0700

    saving model in SL script

--- scripts/sl_on_gamedata.py
+++ scripts/sl_on_gamedata.py
@@ -1,5 +1,8 @@
 from scripts.train_on_generated_gamedata import get_generated_gamedata_dataset
 from scripts.sl_on_policy_head import train_and_plot_datasets, get_n_random_states, create_gtd_from_states
+from a0.model import save_model
+
+import random
 
 from config import config
 
@@ -11,15 +14,17 @@ def main():
         n=None,
         simulated=False,
     )
+    eboards = list(eboards)[:5000]
+    random.shuffle(eboards)
     
     random_states = get_n_random_states(5000)
 
     test_ds = ev_dataset.split_off_test(10000, shuffle=True)
 
-    seen_gt = create_gtd_from_states(list(eboards)[:5000])
+    seen_gt = create_gtd_from_states(eboards)
     random_gt = create_gtd_from_states(random_states)
 
-    train_and_plot_datasets(
+    trained_model = train_and_plot_datasets(
         fn="sl_on_gamedata",
         dataset=ev_dataset,
         eval_datasets={
@@ -29,6 +34,11 @@ def main():
         },
         num_epochs=10,
         res_blocks=3
+    )
+
+    save_model(
+        config.training_dir + "/sl_on_gamedata_model",
+        trained_model
     )
 
 if __name__ == "__main__":
