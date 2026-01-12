86fad987d340cf139fcfc0b69b28c1449ee25858
Nicolas Ong <Nicolas.J.Ong@gmail.com>
2026-01-12 13:53:36 -0700

    gamedata dataset generation now creates a list of NT boards. Training script now uses them.

--- scripts/sl_on_gamedata.py
+++ scripts/sl_on_gamedata.py
@@ -1,6 +1,8 @@
 from scripts.train_on_generated_gamedata import get_generated_gamedata_dataset
 from scripts.sl_on_policy_head import train_and_plot_datasets, get_n_random_states, create_gtd_from_states
 from a0.model import save_model
+from cc.ground_truth import GroundTruth
+from cc.core import Game, Board
 
 import random
 
@@ -9,19 +11,26 @@ from config import config
 from utils.log import get_logger, setup_logging
 logger = get_logger(__name__)
 
+def remove_trivial_boards(boards: list[Board], gt: GroundTruth) -> list[Board]:
+    non_trivial_boards: list[Board] = []
+    for board in boards:
+        if not gt.is_trivial(board):
+            non_trivial_boards.append(board)
+    return non_trivial_boards
+
 def main():
-    ev_dataset, eboards = get_generated_gamedata_dataset(
+    ev_dataset, _, eboards_nt = get_generated_gamedata_dataset(
         n=None,
         simulated=False,
     )
-    eboards = list(eboards)[:5000]
-    random.shuffle(eboards)
+    eboards_nt = list(eboards_nt)[:5000]
+    random.shuffle(eboards_nt)
     
     random_states = get_n_random_states(5000)
 
     test_ds = ev_dataset.split_off_test(10000, shuffle=True)
 
-    seen_gt = create_gtd_from_states(eboards)
+    seen_gt = create_gtd_from_states(eboards_nt)
     random_gt = create_gtd_from_states(random_states)
 
     trained_model = train_and_plot_datasets(
@@ -42,22 +51,22 @@ def main():
     )
 
 def main2():
-    _, eboards = get_generated_gamedata_dataset(
+    _, eboards, _ = get_generated_gamedata_dataset(
         n=None,
         simulated=False,
     )
     eboards = list(eboards)[:100000]
     random.shuffle(eboards)
 
-    train_eboards = eboards[:80000]
-    test_eboards = eboards[80000:]
+    train_eboards = eboards[:90000]
+    test_eboards = eboards[90000:]
     
     random_states = get_n_random_states(5000)
 
     train_ds = create_gtd_from_states(train_eboards)
     test_ds = create_gtd_from_states(test_eboards)
 
-    seen_gt = create_gtd_from_states(train_eboards[:5000])
+    seen_gt = create_gtd_from_states(remove_trivial_boards(train_eboards[:5000], GroundTruth()))
     random_gt = create_gtd_from_states(random_states)
 
     trained_model = train_and_plot_datasets(
