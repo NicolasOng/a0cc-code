2653f31429a4323605e163205ce07747d5bf0d3f
Nicolas Ong <Nicolas.J.Ong@gmail.com>
2026-01-19 17:13:14 -0700

    added test for timing cpu v gpu

--- scripts/cpu_v_gpu.py
+++ scripts/cpu_v_gpu.py
@@ -7,7 +7,7 @@ from a0.players.a0 import A0Player
 from a0.model_utils import board_to_input, get_policy_head_policy, get_value_head_policy
 from a0.eval.dataset_evaluation import evaluate_model
 
-from scripts.sl_on_policy_head import get_n_random_states, create_gtd_from_states
+from scripts.sl_on_policy_head import get_n_random_states, create_gtd_from_states, train_and_plot_datasets
 
 from config import config
 from utils.log import get_logger, setup_logging
@@ -16,12 +16,17 @@ logger = get_logger(__name__)
 def main_parallel() -> None:
     pass
 
-def time_model_inference(n: int = 1000000, model_no: int = 100) -> None:
+def time_model_inference(n: int = 100000, model_no: int = 25) -> None:
     start_time = time.time()
 
     trained_model = load_model(config.training_dir + f"model_{model_no}.pkl", training=False)
     random_states = get_n_random_states(n)
     rgtd = create_gtd_from_states(random_states)
+
+    end_time = time.time()
+    elapsed_time_setup = end_time - start_time
+
+    start_time = time.time()
 
     loss, value_loss, policy_loss, value_accuracy, policy_accuracy = evaluate_model(trained_model, rgtd)
 
@@ -35,21 +40,48 @@ def time_model_inference(n: int = 1000000, model_no: int = 100) -> None:
     end_time = time.time()
     elapsed_time = end_time - start_time
 
+    logger.info(f"Total time for setting up {n} states: {elapsed_time_setup:.2f} seconds")
     logger.info(f"Total time for evaluating {n} states: {elapsed_time:.2f} seconds")
 
+def time_model_training(n: int = 100000) -> None:
+    start_time = time.time()
+
+    random_states = get_n_random_states(n)
+    rgtd = create_gtd_from_states(random_states)
+
+    end_time = time.time()
+    elapsed_time_setup = end_time - start_time
+
+    start_time = time.time()
+
+    train_and_plot_datasets(
+        fn=f"timing_model",
+        dataset=rgtd,
+        eval_datasets={},
+        num_epochs=10,
+        res_blocks=3
+    )
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    logger.info(f"Total time for setting up {n} states: {elapsed_time_setup:.2f} seconds")
+    logger.info(f"Total time for training {n} states: {elapsed_time:.2f} seconds")
+
 def main() -> None:
-    time_model_inference(n=100000, model_no=100)
+    time_model_training(n=100000)
+    time_model_inference(n=100000, model_no=50)
 
 if __name__ == "__main__":
     setup_logging(
         level=20,
         log_dir=config.log_dir,
-        process_name="sl_on_policy_head"
+        process_name="cpu_v_gpu"
     )
 
-    try:
-        multiprocessing.set_start_method('spawn')
-    except RuntimeError:
-        pass
+    # try:
+    #     multiprocessing.set_start_method('spawn')
+    # except RuntimeError:
+    #     pass
 
     main()
